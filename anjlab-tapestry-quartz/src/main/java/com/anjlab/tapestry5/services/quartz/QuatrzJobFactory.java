4cba07f6cd12d5950ffe4ecfa8e55cb8b964a494
dmitrygusev <dmitry.gusev@gmail.com>
2013-09-12 18:39:51 +0400

    Use ObjectLocator instead of Registry in QuartzJobFactory

--- anjlab-tapestry-quartz/src/main/java/com/anjlab/tapestry5/services/quartz/QuatrzJobFactory.java
+++ anjlab-tapestry-quartz/src/main/java/com/anjlab/tapestry5/services/quartz/QuatrzJobFactory.java
@@ -15,12 +15,8 @@
  */
 package com.anjlab.tapestry5.services.quartz;
 
-import javax.servlet.ServletContext;
-
-import org.apache.tapestry5.TapestryFilter;
-import org.apache.tapestry5.ioc.Registry;
+import org.apache.tapestry5.ioc.ObjectLocator;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.services.ApplicationGlobals;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
@@ -33,7 +29,7 @@ import org.quartz.spi.TriggerFiredBundle;
 public class QuatrzJobFactory extends PropertySettingJobFactory implements JobFactory
 {
     @Inject
-    private ApplicationGlobals applicationGlobals;
+    private ObjectLocator objectLocator;
     
     @Override
     public Job newJob(TriggerFiredBundle bundle, Scheduler scheduler)
@@ -42,16 +38,11 @@ public class QuatrzJobFactory extends PropertySettingJobFactory implements JobFa
         JobDetail jobDetail = bundle.getJobDetail();
         Class<? extends Job> jobClass = jobDetail.getJobClass();
         
-        ServletContext servletContext = applicationGlobals.getServletContext();
-        
-        Registry registry = (Registry)
-                servletContext.getAttribute(TapestryFilter.REGISTRY_CONTEXT_NAME);
-        
         Job job;
         
         try
         {
-            job = registry.autobuild(jobClass);
+            job = objectLocator.autobuild(jobClass);
         }
         catch (Exception e)
         {
--- anjlab-tapestry-quartz/src/test/java/com/anjlab/tapestry5/services/quartz/SchedulerTest.java
+++ anjlab-tapestry-quartz/src/test/java/com/anjlab/tapestry5/services/quartz/SchedulerTest.java
@@ -19,21 +19,15 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import javax.servlet.ServletContext;
-
 import junit.framework.Assert;
 
 import org.apache.tapestry5.SymbolConstants;
-import org.apache.tapestry5.TapestryFilter;
-import org.apache.tapestry5.internal.test.PageTesterContext;
 import org.apache.tapestry5.ioc.Registry;
 import org.apache.tapestry5.ioc.RegistryBuilder;
 import org.apache.tapestry5.ioc.services.SymbolSource;
-import org.apache.tapestry5.services.ApplicationGlobals;
 import org.apache.tapestry5.services.TapestryModule;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
@@ -52,19 +46,6 @@ public class SchedulerTest
                     TapestryModule.class,
                     QuartzModule.class)
                 .build();
-
-        ApplicationGlobals globals = registry.getObject(
-                ApplicationGlobals.class, null);
-
-        globals.storeContext(new PageTesterContext(""));
-        
-        ServletContext servletContext = Mockito.mock(ServletContext.class);
-        
-        Mockito.when(servletContext.getAttribute(TapestryFilter.REGISTRY_CONTEXT_NAME))
-               .thenReturn(registry);
-        
-        //  Required for QuartzJobFactory to get an instance of this registry
-        globals.storeServletContext(servletContext);
 
         registry.performRegistryStartup();
     }
