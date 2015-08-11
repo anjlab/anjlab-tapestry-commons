1d0231b5b44dab42d74c70760eda1fd3aab43324
Dmitry Gusev <dmitry.gusev@gmail.com>
2015-08-11 05:28:20 +0300

    Remove recently added SymbolProvider

--- anjlab-tapestry-commons/src/main/java/com/anjlab/tapestry5/services/CommonsModule.java
+++ anjlab-tapestry-commons/src/main/java/com/anjlab/tapestry5/services/CommonsModule.java
@@ -1,9 +1,6 @@
 package com.anjlab.tapestry5.services;
 
-import org.apache.tapestry5.ioc.ObjectLocator;
-import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
-import org.apache.tapestry5.ioc.services.SymbolProvider;
 
 public class CommonsModule
 {
@@ -11,21 +8,5 @@ public class CommonsModule
     {
         binder.bind(InjectionHelper.class);
         binder.bind(ConfigHelper.class);
-    }
-
-    public static void contributeSymbolSource(
-            OrderedConfiguration<SymbolProvider> configuration,
-            ObjectLocator objectLocator)
-    {
-        final ConfigHelper configHelper = objectLocator.getService(ConfigHelper.class);
-
-        configuration.add("ConfigHelper", new SymbolProvider()
-        {
-            @Override
-            public String valueForSymbol(String symbolName)
-            {
-                return configHelper.get(symbolName);
-            }
-        }, "after:SystemProperties", "before:ApplicationDefaults");
     }
 }
